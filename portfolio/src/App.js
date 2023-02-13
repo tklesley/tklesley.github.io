6987028f49ec23903e23350a37ef2e4d288d26c5
tklesley <117387260+tklesley@users.noreply.github.com>
2023-02-13 01:05:51 -0600

    Setting the groundwork for the portfolio website

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,25 +1,63 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends React.Component {
+  render() {
+    return (
+      <div className={`page`}>
+        <Header />
+        <Projects />
+        <Footer />
+      </div>
+  );}
+}
+
+function Header() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`header`}>
+      This is a placeholder for a portfolio website.
     </div>
   );
 }
+
+function Projects() {
+  return (
+    <div className={`project-container`}>
+      Links to projects will go here.
+      <SingleProject />
+    </div>
+  );
+}
+
+function Footer() {
+  return (
+    <div className={`footer`}>
+      Footer
+    </div>
+  )
+}
+
+// -------------------------------------------
+
+function SingleProject() {
+  return (
+    <div className={`project`}>
+      <div className={`project-title`}>
+        Project Title
+      </div>
+      <div className={`project-image`}>
+        A representation of the project
+      </div>
+      <div className={`project-desc`}>
+        A description of the project.
+      </div>
+      <div className={`project-link`}>
+        Link to the project goes here.
+      </div>
+    </div>
+  );
+}
+
+// -------------------- Export ----------------
 
 export default App;
