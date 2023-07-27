28dafbf7d006f9fee3528c3e373e3be0dd5d3693
Keewy <tklesley@outlook.com>
2023-07-27 18:41:40 -0400

    Added Landing Page

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -4,14 +4,19 @@ import React from 'react';
 import Header from './components/header/header';
 import Footer from './components/footer/footer';
 import Projects from './pages/projects/projects';
+import Landing from './pages/landing/landing';
 
 class App extends React.Component {
   render() {
     return (
       <HashRouter>
         <div className={`page`}>
+          Placeholder Text
           <Header />
-          <Projects />
+          <Routes>
+            <Route path='/' element={<Landing />}/>
+            <Projects />
+          </Routes>
           <Footer />
         </div>
       </HashRouter>
---
+++ portfolio/src/pages/landing/landing.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./landing.css";
+
+export default function Landing() {
+    return(
+            <div className="landing-page">
+                <div>Tyler Lesley</div>
+            </div>
+        )
+
+}
