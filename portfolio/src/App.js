c9208d5cf8c415233e4664baa5e51231fddbfa0d
Keewy <tklesley@outlook.com>
2023-08-01 18:47:40 -0400

    recreated a working site

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,62 +1,26 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import React from 'react';
+import Header from './components/header/header';
+import Footer from './components/footer/footer';
+import Landing from './pages/landing/landing';
+import Projects from './pages/projects/projects';
 
 class App extends React.Component {
   render() {
     return (
-      <div className={`page`}>
-        <Header />
-        <Projects />
-        <Footer />
-      </div>
-  );}
-}
-
-function Header() {
-  return (
-    <div className={`header`}>
-      This is a placeholder for a portfolio website.
-    </div>
-  );
-}
-
-function Projects() {
-  return (
-    <div className={`project-container`}>
-      Links to projects will go here.
-      <SingleProject />
-    </div>
-  );
-}
-
-function Footer() {
-  return (
-    <div className={`footer`}>
-      Footer
-    </div>
-  )
-}
-
-// -------------------------------------------
-
-function SingleProject() {
-  return (
-    <div className={`project`}>
-      <div className={`project-title`}>
-        Project Title
-      </div>
-      <div className={`project-image`}>
-        A representation of the project
-      </div>
-      <div className={`project-desc`}>
-        A description of the project.
-      </div>
-      <div className={`project-link`}>
-        Link to the project goes here.
-      </div>
-    </div>
-  );
-}
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Routes>
+            <Route path='' element={<Landing />} />
+            <Route path='/projects' element={<Projects/>} />
+          </Routes>
+          <Footer />
+        </div>
+      </BrowserRouter>
+    )}
+  }
 
 // -------------------- Export ----------------
 
