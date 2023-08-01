f5510b2897f536ff7bf42ef5328f8ed5ee882400
Keewy <tklesley@outlook.com>
2023-08-01 17:43:21 -0400

    Revert "Organization of Components"
    This reverts commit d2b1314dfec459577da737bcf4e8e71c1276eadf.

--- portfolio/src/components/header/header.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Header() {
-    return (
-      <div className={`header`}>
-        This is a placeholder for a portfolio website.
-      </div>
-    );
-  }
