67ffb1e7499f595851746066caeaf5ab42291415
Keewy <tklesley@outlook.com>
2023-08-01 19:17:55 -0400

    Started Header CSS

--- portfolio/src/components/header/header.js
+++ portfolio/src/components/header/header.js
@@ -1,7 +1,12 @@
 import React from "react";
+import './header.css';
 
 export default function Header() {
     return (
-        <div>Header</div>
+        <div className="header-container">
+            <div>Left</div>
+            <div>Middle</div>
+            <div>Right</div>
+        </div>
     )
 }
