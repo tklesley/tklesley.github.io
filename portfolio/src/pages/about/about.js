e1f1c183ee82c56be518da059e4582a0395050c7
Keewy <tklesley@outlook.com>
2023-09-09 01:24:53 -0400

    Filled Out the About Page
    - There's enough there to say I did it.  Last thing I need for the base site.
    - Quick fix on the sidebar so that

--- portfolio/src/pages/about/about.js
+++ portfolio/src/pages/about/about.js
@@ -8,7 +8,17 @@ export default function About() {
             <div className="a-title">About</div>
                 <Sidebar />
                 <div className="about-section">
-                    About Me Goes Here
+                    <p>I'm a web developer working primarily with React.js.  I've gone through the Mimo Web Development bootcamp and have a certificate from doing so.<br/>
+                    Some relevant skills include:</p>
+                    <ul>
+                        <li>HTML, CSS, and Javascript</li>
+                        <li>React.js</li>
+                        <li>Node.js</li>
+                        <li>Google Firebase</li>
+                        <li>Python</li>
+                        <li>GitHub</li>
+                        <li>PC-DMIS</li>
+                    </ul>
                 </div>
         </div>
     )
