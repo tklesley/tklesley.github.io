5694055ffa9368e147b521ac63225c14e8f3ff44
tklesley <117387260+tklesley@users.noreply.github.com>
2024-02-06 06:06:23 -0500

    Updated About Page
    Added more information about what I currently do and some explanation of what PC-DMIS is to keep people from having to move off the page.

--- portfolio/src/pages/about/about.js
+++ portfolio/src/pages/about/about.js
@@ -8,17 +8,24 @@ export default function About() {
             <div className="a-title">About</div>
                 <Sidebar />
                 <div className="about-section">
-                    <p>I'm a web developer working primarily with React.js.  I've gone through the Mimo Web Development bootcamp and have a certificate from doing so.<br/>
+                    <p>I currently work as a PC-DMIS programmer running Hexagon Metrology CMMs.  I've been running CMMs for six years and programming them for roughly two years.  In my free time, I've been working to learn software and web development with the goal of transfering to a new field.<br/><br/>
                     Some relevant skills include:</p>
                     <ul>
+                        <li>PC-DMIS</li>
                         <li>HTML, CSS, and Javascript</li>
                         <li>React.js</li>
                         <li>Node.js</li>
                         <li>Google Firebase</li>
                         <li>Python</li>
                         <li>GitHub</li>
-                        <li>PC-DMIS</li>
+                        <li>C#</li>
                     </ul>
+                    <h2>Certifications</h2>
+                    <ul>
+                        <li>Mimo Web Development Certification</li>
+                    </ul>
+                    <h2>About PC-DMIS Programming</h2>
+                    <p>PC-DMIS is a software used for running Coordinate Measurement Machines, or CMMs.  Programming in PC-DMIS involves writing programs that move the CMM around in 3D space to take measurements on various parts with high accuracy, usually within a micron.  In most cases, you can use a CAD model to assist in programming.  Some skills learned through my experience programming in PC-DMIS include interpreting undocumented code from previous programers, troubleshooting issues, debugging, reading engineering documentation, and editing programs.</p>
                 </div>
         </div>
     )
