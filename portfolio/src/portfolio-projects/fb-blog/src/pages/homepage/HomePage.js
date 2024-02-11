a11c2190b68020b4f9f89bce78a54fad50f42a28
tklesley <117387260+tklesley@users.noreply.github.com>
2024-02-11 15:02:54 -0500

    Moved Editor's Choice to Components
    Also changed banner.css height to not be 50vh as that was causing an overlap when the content exceeded 50vh.

--- portfolio/src/portfolio-projects/fb-blog/src/pages/homepage/HomePage.js
+++ portfolio/src/portfolio-projects/fb-blog/src/pages/homepage/HomePage.js
@@ -1,47 +1,16 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Banner from '../../components/Banner/Banner';
-import { db } from '../../config/firebaseConfig';
-import { getDocs, collection, query, where, limit } from 'firebase/firestore';
+import Selected from '../../components/Selected/selected';
+
 import './home.css'
-import { useNavigate } from 'react-router-dom';
 
 
 export default function HomePage() {
 
-    const navigate = useNavigate();
-    const [selectedArticles, setSelectedArticles] = useState([]);
-
-    useEffect(()=>{
-        const articleRef = collection(db, "articles");
-        const q = query(articleRef, where("isSelected", "==",  true), limit(5))
-        getDocs(q, articleRef)
-        .then(res=>{
-            const articles = res.docs.map(item=>({
-                id: item.id,
-                ...item.data()
-            }))
-            setSelectedArticles(articles)
-        })
-    })
-
     return (
         <div className='home-page-layout'>
             <Banner />
-            <div className="selected-articles">
-                <h1>Editor's Choice</h1>
-                {
-                    selectedArticles?.map(item=>{
-                        return (
-                            <div className='selected-article-item' key={item.id} onClick={()=>navigate(`/article/${item.id}`)} >
-                                <img src={item.imageUrl} alt={item.title} className='selected-image' />
-                                <div className='selected-info'>
-                                    <h2>{item.title}</h2>
-                                    <p>{item.summary}</p>
-                                </div>
-                            </div>
-                            )})
-                }
-            </div>
+            <Selected />
         </div>
     )
 }
